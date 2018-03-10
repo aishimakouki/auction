ab81c60389b7533edcbb1ab506d56c3ca25519c6
kouki <miki.0418.kouki@gmail.com>
2018-03-10 15:27:13 +0900

    オークションアプリ完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,37 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  def edit
+      #Itemモデルのfindメソッドを使用して、パラメータに値に一致するidのデータを検索している
+      #取得したデータを@itemという変数にセットする
+      @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to "/items/#{@item.id}"
+  end
+
+  def destroy
+    @item =Item.find(params[:id])
+    @item.destroy
+    redirect_to "/items"
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 end
